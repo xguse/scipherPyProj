b125fcb78321142e0d70d3ca81521ba7a5aef49b
Augustine Dunn <wadunn83@gmail.com>
2010-12-01 21:44:13 -0800

    added: scipherSrc/defs/markovModels.py (NOT IMPLEMENTED YET!)

---
+++ scipherSrc/defs/markovModels.py
@@ -0,0 +1,7 @@
+
+class nOrdMkvBkg(object):
+    """"""
+    def __init__(self,order,seqList):
+        """Returns a dict representing the 'order'^th order background
+        model of 'order' length substrings in 'seqList'."""
+        raise NotImplementedError()
