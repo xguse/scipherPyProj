d38a7789afda7395933fd9417e05c7106a1f9ea1
Augustine Dunn <wadunn83@gmail.com>
2011-06-15 07:32:03 -0700

    scipherSrc/scripts/xls2csv.py: -changed to default to print rows to stdOut if no outPath provided

--- scipherSrc/scripts/xls2csv.py
+++ scipherSrc/scripts/xls2csv.py
@@ -1,19 +1,21 @@
 import sys,optparse
 from scipherSrc.defs.files_io import xls2csv
 def main():
-    usage = """python %prog [options] xlsPath outPath"""
+    usage = """python %prog [options] xlsPath [outPath]"""
     parser = optparse.OptionParser(usage)
-    parser.add_option('--delim',type="str", default=",", 
+    parser.add_option('-d','--delim',type="str", default=",", 
                       help="""The text deleminator you wish to use. (default=%default)""")
     
     (opts, args) = parser.parse_args()
         
     if len(sys.argv[1:]) == 0:
-        parser.print_usage()
+        parser.print_help()
         exit(0)
-    if not len(args) == 2:
-        raise Exception("**ERROR: you must supply both an xlsPath and outPath.**")
-    
+    if not len(args) >= 1:
+        raise Exception("**ERROR: you must supply at least an xlsPath.**")
+    if len(args) == 1:
+        args.append(None)
+        
     xls2csv(args[0],args[1],sep=opts.delim)
     
 if __name__ == "__main__":
